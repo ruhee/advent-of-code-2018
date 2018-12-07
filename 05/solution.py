15d9ac358a3efd25f36b41e7f67596b8012e4555
Ruhee Dewji <ruhee.dewji@gmail.com>
2018-12-06 22:37:33 -0500

    5.1

---
+++ 05/solution.py
@@ -0,0 +1,26 @@
+#! /usr/local/bin/python3
+
+def react(input):
+  for idx, char in enumerate(input):
+    if idx < len(input) - 1: # whoops
+      next_char = input[idx+1]
+      if (char.lower() == next_char.lower() and
+        char != next_char):
+        return input[:idx] + input[(idx+2):]
+  return input
+
+with open('input.txt', 'r') as input:
+  data = input.read().strip().rstrip('\n')
+
+  # test_input = 'dabAcCaCBAcCcaDA'
+
+  init_input = data
+  complete = False
+
+  while not complete:
+    count = len(init_input)
+    init_input = react(init_input)
+
+    if count == len(init_input):
+      complete = True
+      print(len(init_input))
