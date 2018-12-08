c3051d1e7ce59127b4c6bcfe04d07f9b8203ad05
Ruhee Dewji <ruhee.dewji@gmail.com>
2018-12-07 22:17:11 -0500

    5.2

--- 05/solution.py
+++ 05/solution.py
@@ -12,15 +12,32 @@ def react(input):
 with open('input.txt', 'r') as input:
   data = input.read().strip().rstrip('\n')
 
-  # test_input = 'dabAcCaCBAcCcaDA'
+  test_input = 'dabAcCaCBAcCcaDA'
 
   init_input = data
-  complete = False
+  # complete = False
 
-  while not complete:
-    count = len(init_input)
-    init_input = react(init_input)
+  # while not complete:
+  #   count = len(init_input)
+  #   init_input = react(init_input)
 
-    if count == len(init_input):
-      complete = True
-      print(len(init_input))
+  #   if count == len(init_input):
+  #     complete = True
+  #     print(len(init_input))
+
+  # part 2
+  reacted = {}
+  for alpha in 'abcdefghijklmopqrstuvwxyz':
+    stripped = init_input.replace(alpha, '').replace(alpha.upper(), '')
+
+    complete = False
+
+    while not complete:
+      count = len(stripped)
+      stripped = react(stripped)
+
+      if count == len(stripped):
+        complete = True
+        reacted[alpha] = len(stripped)
+        print(alpha, len(stripped))
+  print(reacted)
